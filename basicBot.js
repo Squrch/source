f7b75fc2f180c6967d748e39c445c35a905a62ad
Squrch <squrchgraphics@gmail.com>
2016-09-05 15:50:15 +0100

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -99,7 +99,7 @@
     var loadChat = function (cb) {
         if (!cb) cb = function () {
         };
-        $.get("https://rawgit.com/squrch/source/master/lang/langIndex.json", function (json) {
+        $.get("https://rawgit.com/basicbot/source/master/lang/langIndex.json", function (json) {
             var link = basicBot.chatLink;
             if (json !== null && typeof json !== "undefined") {
                 langIndex = json;
@@ -240,9 +240,9 @@
         status: false,
         name: "basicBot",
         loggedInID: null,
-        scriptLink: "https://rawgit.com/squrch/source/master/basicBot.js",
+        scriptLink: "https://rawgit.com/basicbot/source/master/basicBot.js",
         cmdLink: "http://git.io/245Ppg",
-        chatLink: "https://rawgit.com/squrch/source/master/lang/en.json",
+        chatLink: "https://rawgit.com/basicbot/source/master/lang/en.json",
         chat: null,
         loadChat: loadChat,
         retrieveSettings: retrieveSettings,
@@ -250,8 +250,8 @@
         settings: {
             botName: "basicBot",
             language: "english",
-            chatLink: "https://rawgit.com/squrch/source/master/lang/en.json",
-            scriptLink: "https://rawgit.com/squrch/source/master/basicBot.js",
+            chatLink: "https://rawgit.com/basicbot/source/master/lang/en.json",
+            scriptLink: "https://rawgit.com/basicbot/source/master/basicBot.js",
             roomLock: false, // Requires an extension to re-load the script
             startupCap: 200, // 1-200
             startupVolume: 0, // 0-100
